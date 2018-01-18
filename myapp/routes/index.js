37dfe7a443f56506dfdbc3063832cee6603bb480
iCoobin <15757117989@163.com>
2018-01-18 15:05:35 +0800

    update

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/ios',function(req, res, next){
-  // res.render('index', { title: 'Express' });
-  res.end("<script>window.location.href = 'https://itunes.apple.com/app/id1112879717?mt=8';</script>")
+  res.render('index', { title: 'Express' });
+  // res.end("<script>window.location.href = 'https://itunes.apple.com/app/id1112879717?mt=8';</script>")
 });
 
 module.exports = router;
