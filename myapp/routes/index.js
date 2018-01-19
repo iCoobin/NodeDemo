22f833eac046a732f6c82437377d93035cdb5772
iCoobin <15757117989@163.com>
2018-01-19 11:13:38 +0800

    update

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.end("<a href='https://ios.ejufu.com'>跳转连接</a>");
 });
 
 router.get('/ios',function(req, res, next){
